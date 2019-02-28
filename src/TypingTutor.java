c146b4a54af7edd6e9977047abeee88c8e66ac9b
kiernan.victor <kiernan.victor@SEMS-259-20.SEMS259.LAB>
2019-02-27 16:29:56 -0800

    typing tutor ip

--- src/TypingTutor.java
+++ src/TypingTutor.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Random;
@@ -9,31 +11,46 @@ import javax.swing.JPanel;
 public class TypingTutor implements KeyListener {
 	char currentLetter;
 	String letter;
+	JFrame frame;
+	 Random r;
+	 JPanel panel;
 public static void main(String[] args) {
 	TypingTutor tutor = new TypingTutor();
 	tutor.TutorMaker();
 	
 	}
 public void TutorMaker() {
-	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
+	frame = new JFrame();
+	 panel = new JPanel();
 	JLabel label = new JLabel();
-	label.setText(letter);
-	label.setFont(label.getFont().deriveFont(28.0f));
 	frame.setVisible(true);
 	frame.addKeyListener(this);
 	frame.add(panel);
 	panel.add(label);
 	currentLetter = generateRandomLetter();
 	letter=Character.toString(currentLetter);
+	label.setFont(label.getFont().deriveFont(750.0f));
+	label.setHorizontalAlignment(JLabel.CENTER);
+	label.setText(letter);
+	System.out.println(letter);
 	frame.pack();
 	}
+
+	
 char generateRandomLetter() {
-      Random r = new Random();
+       r = new Random();
       return (char) (r.nextInt(26) + 'a');
 }
 public void keyPressed(KeyEvent arg0) {
-	// TODO Auto-generated method stub
+char keycode=arg0.getKeyChar();
+System.out.println(keycode);
+System.out.println(currentLetter);
+if(keycode==currentLetter) {
+	panel.setBackground(Color.green);
+}
+else {
+	panel.setBackground(Color.red);
+}
 }
 public void keyReleased(KeyEvent arg0) {
 	// TODO Auto-generated method stub
