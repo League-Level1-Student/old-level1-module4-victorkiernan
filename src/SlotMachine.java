f916ca9633577e71628b8745da9ac16049a56dd0
kiernan.victor <kiernan.victor@SEMS-259-29.SEMS259.LAB>
2019-04-24 16:34:43 -0700

    slotmachine ip

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -1,8 +1,11 @@
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
 import java.util.Random;
 
+import javax.print.DocFlavor.URL;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,123 +13,103 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class SlotMachine implements ActionListener  {
-public static void main(String[] args) {
-	SlotMachine slot = new SlotMachine(500,300);
-	
-}
-JFrame frame = new JFrame();
-JPanel middlespinner = new JPanel();
-JPanel bottombutton = new JPanel();
-JPanel panelname = new JPanel();
-JLabel titlename = new JLabel();
-JButton button = new JButton("Spin");
-JLabel label1 = new JLabel();
-JLabel label2 = new JLabel();
-JLabel label3 = new JLabel();
-Random gen = new Random();
-public SlotMachine(int width, int height) {
-	frame.setVisible(true);
-	frame.add(panelname, BorderLayout.NORTH);
-	frame.add(middlespinner, BorderLayout.CENTER);
-	frame.add(bottombutton, BorderLayout.SOUTH);
-	middlespinner.add(label1);
-	titlename.setText("The Game You Will Never Win... I Mean You Could So Good Luck To You");
-	panelname.add(titlename);
-	middlespinner.add(label2);
-	middlespinner.add(label3);
-	bottombutton.add(button);
-	frame.setSize(500, 500);
-	label1.setText("1");
-	label2.setText("2");
-	label3.setText("3");
-	button.addActionListener(this);
-}
-@Override
-public void actionPerformed(ActionEvent arg0) {
-	int num1 = gen.nextInt(3)+1;
-	label1.setText(""+num1);
-	if(num1==-1) {
-		 ImageIcon icon = new ImageIcon("Cherries-512.png");
-		  JLabel label = new JLabel(icon);
-		 middlespinner.add(label);
-		 middlespinner.setVisible(true);
-	}
-	else if(num1==2) {
-		 ImageIcon icon = new ImageIcon("");
-		  JLabel label = new JLabel(icon);
-		 middlespinner.add(label);
-		 middlespinner.setVisible(true);
-	}
-	else {
-		 ImageIcon icon = new ImageIcon("Cherries-512.png");
-		  JLabel label = new JLabel(icon);
-		middlespinner.add(label);
-		 middlespinner.setVisible(true);
-	}
-	int num2 = gen.nextInt(3)+1;
-	label2.setText(""+num2);
-	if(num2==-1) {
-		 ImageIcon icon = new ImageIcon("Cherries-512.png");
-		  JLabel label = new JLabel(icon);
-		  middlespinner.add(label);
-			 middlespinner.setVisible(true);
-	}
-	else if(num2==2) {
-		 ImageIcon icon = new ImageIcon("");
-		  JLabel label = new JLabel(icon);
-		middlespinner.add(label);
-		 middlespinner.setVisible(true);
-	}
-	else {
-		 ImageIcon icon = new ImageIcon("Cherries-512.png");
-		  JLabel label = new JLabel(icon);
-		 middlespinner.add(label);
-		 middlespinner.setVisible(true);
-	}
-	int num3= gen.nextInt(3)+1;
-	label3.setText(""+num3);
-	if(num3==-1) {
-		 ImageIcon icon = new ImageIcon("Cherries-512.png");
-		  JLabel label = new JLabel(icon);
-		 middlespinner.add(label);
-		 middlespinner.setVisible(true);
-	}
-	else if(num3==2) {
-		 ImageIcon icon = new ImageIcon("");
-		  JLabel label = new JLabel(icon);
-		middlespinner.add(label);
-		 middlespinner.setVisible(true);
-	}
-	else {
-		 ImageIcon icon = new ImageIcon("Cherries-512.png");
-		  JLabel label = new JLabel(icon);
-		middlespinner.add(label);
-		 middlespinner.setVisible(true);
-	}
-	if(checkWin(num1, num2, num3) == true) {
-	JOptionPane.showMessageDialog(null, "You won!");}
-	
-}
+public class SlotMachine implements ActionListener {
+	public static void main(String[] args) {
+		SlotMachine slot = new SlotMachine(500, 300);
 
-	boolean checkWin(int a,int b, int c) {
-		if(a==b&&b==c) {
-			return true;
-		
+	}
+
+	JFrame frame = new JFrame();
+	JPanel middlespinner = new JPanel();
+	JPanel bottombutton = new JPanel();
+	JPanel panelname = new JPanel();
+	JLabel titlename = new JLabel();
+	JButton button = new JButton("Spin");
+	JLabel label1 = new JLabel();
+	JLabel label2 = new JLabel();
+	JLabel label3 = new JLabel();
+	Random gen = new Random();
+
+	private JLabel createLabelImage(String fileName) throws MalformedURLException {
+		java.net.URL imageURL = getClass().getResource(fileName);
+		if (imageURL == null) {
+			System.err.println("Could not find image " + fileName);
+			return new JLabel();
 		}
-		else {
+		Icon icon = new ImageIcon();
+		JLabel imageLabel = new JLabel(icon);
+		return imageLabel;
+	}
+
+	public SlotMachine(int width, int height) {
+		frame.setVisible(true);
+		frame.add(panelname, BorderLayout.NORTH);
+		frame.add(middlespinner, BorderLayout.CENTER);
+		frame.add(bottombutton, BorderLayout.SOUTH);
+		titlename.setText("The Game You Will Never Win... I Mean You Could, Sooooo Good Luck To You");
+		panelname.add(titlename);
+		middlespinner.add(label1);
+		middlespinner.add(label2);
+		middlespinner.add(label3);
+		bottombutton.add(button);
+		frame.setSize(500, 500);
+		label1.setText("1");
+		label2.setText("2");
+		label3.setText("3");
+		button.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		spinreels();
+
+	}
+
+	public void spinreels() {
+		int[] spinner = new int[3];
+		middlespinner.removeAll();
+		for (int i = 0; i < 3; i++) {
+			int num1 = gen.nextInt(3);
+			System.out.println(num1);
+			spinner[i] = num1;
+			if (num1 == 0) {
+				try {
+					middlespinner.add(createLabelImage("Cherries-512.png"));
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else if (num1 == 1) {
+				try {
+					middlespinner.add(createLabelImage("index.png"));
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else {
+				try {
+					middlespinner.add(createLabelImage("slot-machine-clipart-number-7.png"));
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+		}
+
+		middlespinner.revalidate();
+		if (checkWin(spinner[0], spinner[1], spinner[2]) == true) {
+			JOptionPane.showMessageDialog(null, "You won!");
+		}
+	}
+
+	boolean checkWin(int a, int b, int c) {
+		if (a == b && b == c) {
+			return true;
+
+		} else {
 			return false;
 		}
 	}
-	
+
 }
-
-	
-
-
-
-
-
-
-
-
